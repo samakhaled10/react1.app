a7136f0aec028a9a75ae37202f4f58fdafa50153
samakhaled10 <samasimo2003104@gmail.com>
2024-08-05 21:46:18 +0300

    react3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 
+
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './Components/Layout/Layout';
@@ -8,14 +9,21 @@ import About from './Components/About/About';
 import Portfolio from './Components/Portfolio/Portfolio';
 import Contact from './Components/Contact/Contact';
 
+// Define the basename
+const basename = '/react1.app'; // Use your repository name here
+
 const router = createBrowserRouter([
-  { path: '', element: <Layout />, children: [
-    { path: 'home', element: <Home /> },
-    { path: 'about', element: <About /> },
-    { path: 'portfolio', element: <Portfolio /> },
-    { path: 'contact', element: <Contact /> },
-  ]}
-]);
+  {
+    path: '',
+    element: <Layout />,
+    children: [
+      { path: 'home', element: <Home /> },
+      { path: 'about', element: <About /> },
+      { path: 'portfolio', element: <Portfolio /> },
+      { path: 'contact', element: <Contact /> },
+    ],
+  },
+], { basename });
 
 export default function App() {
   return (
